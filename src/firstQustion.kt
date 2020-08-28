7134972a4dbb1595daaa8cbc8838cb056f1f3058
haytham taha alsharany <talsharany@gmail.com>
2020-08-28 17:24:33 +0300

    haytham commit2

--- src/firstQustion.kt
+++ src/firstQustion.kt
@@ -2,14 +2,15 @@ fun main() {
     var myList= listOf("haytham","waheeb","omar","samar","wafa")
     var myArray= arrayOf("abdallah","rasheed","roqia","maimona")
     var myMutableList= mutableListOf("mokhtar  ghaleb","haitham ameen","eman maktari")
+    println("*****************____________________________________________________**********************")
     for( (index,value) in myArray.withIndex()){
         println("in the index :"+"$index"+"the name is "+"$value")
     }
-    println("-----------------------------------------------------------------------------------------------")
+    println("*****************____________________________________________________**********************")
     for( item in myList.iterator()){
         println("$item")
     }
-    println("-----------------------------------------------------------------------------------------------")
+    println("*****************____________________________________________________**********************")
     for( element in myMutableList.indices){
         println(myMutableList[element])
     }
